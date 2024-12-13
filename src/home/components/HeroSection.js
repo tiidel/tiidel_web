7e16a3632b31cb3ed5a66d05fb3d2faeb2cc6ee8
kimbi619 <kimbidarl@gmail.com>
2024-12-13 15:48:30 -0500

    modifying landing page

--- src/home/components/HeroSection.js
+++ src/home/components/HeroSection.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import Hero1 from './hero_01.jpg'
-
+import Hero2 from './hero_two.png'
+import Hero3 from './hero_three.png'
+import Hero4 from './hero_four.jpg'
+import Hero5 from './hero_five.jpg'
 const slides = [
   {
     title: 'MTN Group Q3 2024',
     subtitle: 'Trading Update',
-    image: Hero1,
+    image: Hero4,
   },
   {
     title: 'Digital Solutions',
     subtitle: 'Transforming Tomorrow',
-    image: Hero1,
+    image: Hero2,
   },
   {
     title: 'Innovation First',
     subtitle: 'Leading Change',
-    image: 'https://via.placeholder.com/800x600.png?text=Innovation+First',
+    image: Hero3,
   },
   {
     title: 'Connected Future',
     subtitle: 'Building Together',
-    image: 'https://via.placeholder.com/800x600.png?text=Connected+Future',
+    image: Hero5,
   },
 ];
 
@@ -69,11 +72,11 @@ export default function HeroSection() {
         </div>
 
         <div className="image-content">
-          <img
+          {/* <img
             src={slides[prevSlide].image}
             alt="Previous hero image"
             className={`hero-image ${isAnimating ? 'fade-out' : ''}`}
-          />
+          /> */}
           <img
             src={slides[currentSlide].image}
             alt="Current hero image"
