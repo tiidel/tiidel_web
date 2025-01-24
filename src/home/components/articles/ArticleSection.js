b60c681b864b88884be6f370c6db8b7a0af3bed5
APEX <kimbidarl@gmail.com>
2025-01-24 10:13:46 -0500

    feat:: added one back link

--- src/home/components/articles/ArticleSection.js
+++ src/home/components/articles/ArticleSection.js
@@ -16,7 +16,7 @@ const ArticleSection = () => {
           <span className="news-date">10 December 2024</span>
           <h2>Investment guide. Secure your future</h2>
           <p>The digital age offers remarkable opportunities for children across Africa.</p>
-          <a href="#" className="read-more">READ MORE</a>
+          <a href="https://blog.tiidel.com/how-investment-works-unlocking-the-world-of-stocks-and-equity-with-tiidel" className="read-more">READ MORE</a>
         </div>
       </div>
       <div className="news-sections">
@@ -28,7 +28,7 @@ const ArticleSection = () => {
             <span className="news-date">28 November 2024</span>
             <h2>Metrices used by investors</h2>
             <p>We took time to document all factors used in documentation</p>
-            <a href="#" className="read-more">READ MORE</a>
+            <a href="https://blog.tiidel.com/" className="read-more">READ MORE</a>
             </div>
         </div>
         <div className="news-section">
@@ -39,7 +39,7 @@ const ArticleSection = () => {
             <span className="news-date">25 November 2024</span>
             <h2>MTN Group extends the magic </h2>
             <p>MTN Group is pleased to announce the official commercial launch of 5G services in Benin and Congo-Brazzaville, bringing the latest advancements in telecommunications to these markets.</p>
-            <a href="#" className="read-more">READ MORE</a>
+            <a href="https://blog.tiidel.com/" className="read-more">READ MORE</a>
           </div>
         </div>
       </div>
